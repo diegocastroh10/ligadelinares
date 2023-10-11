59f284e563059167662fb96bd095d6413741ad37
Diego Castro Herrera <diegocastroh10@hotmail.com>
2023-10-10 23:13:35 -0300

    router pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioAdminComponent } from './pages/inicio-admin/inicio-admin.component';
+import { EquiposComponent } from './pages/equipos/equipos.component';
+import { NoticiasComponent } from './pages/noticias/noticias.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: InicioAdminComponent},
+  { path: 'equipos', component: EquiposComponent},
+  { path: 'noticias', component: NoticiasComponent},
+  //path que redirecciona a la dirección indicada si el link está mal escrito
+  { path: '**', pathMatch: 'full', redirectTo: ''}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+//ROUTES
 import { AppRoutingModule } from './app-routing.module';
+
+//COMPONENTS
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './header/navbar/navbar.component';
 import { FootpageComponent } from './footer/footpage/footpage.component';
 import { InicioAdminComponent } from './pages/inicio-admin/inicio-admin.component';
+import { EquiposComponent } from './pages/equipos/equipos.component';
+import { NoticiasComponent } from './pages/noticias/noticias.component';
+import { UsuariosComponent } from './pages/usuarios/usuarios.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FootpageComponent,
-    InicioAdminComponent
+    InicioAdminComponent,
+    EquiposComponent,
+    NoticiasComponent,
+    UsuariosComponent
   ],
   imports: [
     BrowserModule,
